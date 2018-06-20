50a79176495b07e1243a82046e7f2c6c2bb6e0df
Taylor Wong <taylorwong159@gmail.com>
2018-06-20 07:10:47 -0600

    fixd line

--- index.js
+++ index.js
@@ -13,43 +13,7 @@ function init() {
 };
 
 app.use(express.static(path.join(__dirname, 'Frontend')));
-
 app.use(bodyParser.urlencoded({ extended: true }));
-/*
-app.get('/home', (req, res) => {
-  res.sendFile(path.join(__dirname + '/Frontend/home/index.html'));
-});
-app.get('/home.css', (req, res) => {
-  res.sendFile(path.join(__dirname + '/Frontend/home/styles.css'));
-});
-app.get('/home.js', (req, res) => {
-  res.sendFile(path.join(__dirname + '/Frontend/home/main.js'));
-});
-app.get('/global_styles.css', (req, res) => {
-  res.sendFile(path.join(__dirname + '/Frontend/global_styles.css'));
-});
-app.get('/submit', (req, res) => {
-  res.sendFile(path.join(__dirname + '/Frontend/submit/index.html'));
-});
-app.get('/submit.css', (req, res) => {
-  res.sendFile(path.join(__dirname + '/Frontend/submit/styles.css'));
-});
-app.get('/submit.js', (req, res) => {
-  res.sendFile(path.join(__dirname + '/Frontend/submit/main.js'));
-});
-app.get('/global.js', (req, res) => {
-  res.sendFile(path.join(__dirname + '/Frontend/global_scripts.js'));
-});
-app.get('/profile', (req, res) => {
-  res.sendFile(path.join(__dirname + '/Frontend/profile/index.html'));
-});
-app.get('/profile.css', (req, res) => {
-  res.sendFile(path.join(__dirname + '/Frontend/profile/styles.css'));
-});
-app.get('/profile.js', (req, res) => {
-  res.sendFile(path.join(__dirname + '/Frontend/profile/main.js'));
-});
-*/
 
 MongoClient.connect(db.url, (err, database) => {
   if (err) return console.log(err);
