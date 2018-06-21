be13450761163707ff632265141c58c015b0f3fa
Taylor Wong <taylorwong159@gmail.com>
2018-06-20 21:26:28 -0600

    added

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('./config/db');
 const cookieParser =  require('cookie-parser');
 const fs = require('fs');
 const jsonParser = bodyParser.json();
-
+const port = process.env.PORT || 3000;
 var app = express();
 
 function init() {
@@ -19,5 +19,5 @@ app.use(bodyParser.urlencoded({ extended: true }));
 MongoClient.connect(db.url, (err, database) => {
   if (err) return console.log(err);
   require('./app/routes/routes')(app, database);
-  app.listen(3000, init);
+  app.listen(port, init);
 });
